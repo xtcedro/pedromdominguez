539d3e8e86e11ceb148092bbd7478d90727f5254
xtcedro <pedro.dfedro@gmail.com>
2025-07-09 19:37:50 -0500

    update

--- routes/systemRoutes.ts
+++ routes/systemRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from "https://deno.land/x/oak/mod.ts";
 import { getSystemInfo } from "../controllers/systemController.ts";
 
 const router = new Router();
+
 router.get("/info", getSystemInfo);
 
 export default router;
